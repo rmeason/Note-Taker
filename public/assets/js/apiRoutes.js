886d1b5400750b59691707590d4e5b10fc830503
Richard Michael Eason <rme15a@acu.edu>
2020-06-08 01:45:44 -0500

    added js files

---
+++ public/assets/js/apiRoutes.js
@@ -0,0 +1,27 @@
+const dbData = require("../../../db/db.json");
+
+module.exports = function(app) {
+
+  app.get("/api/notes", function(req, res) {
+    res.json(dbData);
+  });
+
+  app.post("/api/notes", function(req, res) {
+
+    if (dbData.length < 5) {
+      dbData.push(req.body);
+      res.json(true);
+    }
+    else {
+      waitListData.push(req.body);
+      res.json(false);
+    }
+  });
+
+  app.delete("/api/notes/:id", function(req, res) {
+
+    dbData.length = 0;
+
+    res.json({ ok: true });
+  });
+};
--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const PORT = process.env.PORT || 5000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./public/assets/js/apiRoutes")(app);
+require("./public/assets/js/htmlRoutes")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
